1b011a32448f3403f0935aef302e3fd5ac415290
main <ialreadyblah5@gmail.com>
2020-12-23 02:39:11 -0600

    C:/Program Files/Git/ removed

--- scripts/app.js
+++ scripts/app.js
@@ -28,9 +28,9 @@ const updateUI = (data) => {
 
     let timeSrc = null;
     if (weather.IsDayTime) {
-        timeSrc = '/img/day.svg';
+        timeSrc = 'img/day.svg';
     } else {
-        timeSrc = '/img/night.svg';
+        timeSrc = 'img/night.svg';
     }
 
     time.setAttribute('src', timeSrc);
